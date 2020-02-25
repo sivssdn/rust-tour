7a7fe59745992be97406e56ab83a9082dbcf1ff5
Paras <sivssdn@gmail.com>
2020-02-25 23:13:48 +0530

    git pages

---
+++ docs/precache-manifest.afd42cc81ef5a388fdb0db02415f4e27.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86b0f73dec82fa7ea9a443bdcd61de30",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6f10772b1cbe2dd06f89",
+    "url": "./static/css/main.8f344490.chunk.css"
+  },
+  {
+    "revision": "474886b7ca2c00cc2f8d",
+    "url": "./static/js/2.9ba4c084.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.9ba4c084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f10772b1cbe2dd06f89",
+    "url": "./static/js/main.befc78b2.chunk.js"
+  },
+  {
+    "revision": "32751d03ee37366e4933",
+    "url": "./static/js/runtime-main.459c4eb1.js"
+  }
+]);
